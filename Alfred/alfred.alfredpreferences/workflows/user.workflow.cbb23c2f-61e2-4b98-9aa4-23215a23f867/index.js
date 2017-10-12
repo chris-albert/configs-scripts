f43e3df3b6bb3b471f803911e366d51471eb0dcb
Chris Albert <chris.albert@creditkarma.com>
2017-10-12 11:36:55 -0700

    Adding new stuff

---
+++ Alfred/alfred.alfredpreferences/workflows/user.workflow.cbb23c2f-61e2-4b98-9aa4-23215a23f867/index.js
@@ -0,0 +1,47 @@
+const fsp = require('fs-promise');
+const _   = require('lodash');
+
+const directoriesToRead = [
+  '/Users/creasetoph/git'
+];
+
+const dirContents = _.map(directoriesToRead,dir => {
+  return fsp.readdir(dir)
+    .then(dirs => {
+      return _.map(dirs,d => {
+        return {
+          name: d,
+          path: (dir + '/' + d)
+        };
+      })
+    });
+})
+
+
+function getProjectDirs() {
+  return Promise.all(dirContents)
+    .then(dirs => {
+      return _.filter(_.flatten(dirs),d => !_.startsWith(d,'.'));
+    })
+}
+
+function generateAlfredJson(directories) {
+  const items = _.map(directories,directory => {
+    return {
+      title: directory.name,
+      uid: directory.name,
+      arg: directory.path
+    }
+  });
+  return {
+    items: items
+  };
+}
+
+function printJson(json) {
+  console.log(JSON.stringify(json));
+}
+
+getProjectDirs()
+  .then(generateAlfredJson)
+  .then(printJson);
